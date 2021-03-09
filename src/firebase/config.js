a311db547ffc6151f073a2383e4aa71a3e0a3a69
wanko <kouji124@w4.dion.ne.jp>
2021-03-09 00:07:59 -0800

    added config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// export const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+//   };
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyCczd5YbgWZ6hHeqLgV7UpxM7qPys8flQs",
+    authDomain: "react-practice-auth.firebaseapp.com",
+    projectId: "react-practice-auth",
+    storageBucket: "react-practice-auth.appspot.com",
+    messagingSenderId: "509643840778",
+    appId: "1:509643840778:web:5a01d9964e1089c0352ef3",
+    measurementId: "G-ZVPD3C21ZJ"
+  };
